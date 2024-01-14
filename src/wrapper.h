59df690cd0fc67d06f255edc13fd78d95069f25c
SeaEagle1 <seaeagle1@users.sourceforge.net>
2024-01-14 22:26:19 +0100

    Add bindgen to write ffi for libjxl

---
+++ src/wrapper.h
@@ -0,0 +1,7 @@
+#ifndef RIMAGE_WRAPPER_H
+#define RIMAGE_WRAPPER_H
+
+include "libjxl/lib/include/jxl/encode.h"
+include "libjxl/lib/include/jxl/thread_parallel_runner.h"
+
+#endif //RIMAGE_WRAPPER_H
